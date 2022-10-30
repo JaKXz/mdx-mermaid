ea5e69b7fe391828155dd072e9870ce34e5dc31f
Jason Kurian <jason.kurian@shopify.com>
2022-10-30 16:55:39 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,13 +8,5 @@ export default {
     },
   },
   testEnvironment: "jsdom",
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   roots: ["src"],
 };
